5f4c324f53ca4f6382ec3f92c2f82b8011c2aa80
George Whitcher <george@georgewhitcher.com>
2014-03-04 22:30:28 -0500

    Fixed blog and authoriziation.

---
+++ blog/Controllers/PageController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace blog.Controllers
+{
+    public class PageController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contact";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Title = "About";
+            return View("About");
+        }
+    }
+}
---
+++ blog/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace MvcBlog.Models
+{
+    public class Blog
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Slug { get; set; }
+        public DateTime Date { get; set; }
+        public string Body { get; set; }
+    }
+
+    public class BlogDBContext : DbContext
+    {
+        public DbSet<Blog> Blog { get; set; }
+    }
+}
